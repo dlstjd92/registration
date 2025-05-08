bce778eeedb2f22d4a203719ab4ddbcec87ff59d
인성 <a1111@Insparks-MacBook-Pro.local>
2025-05-08 20:37:17 +0900

    hello world

---
+++ src/main/java/com/inspark/courseregistrationmodule/CourseRegistrationModuleApplication.java
@@ -0,0 +1,13 @@
+package com.inspark.courseregistrationmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRegistrationModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseRegistrationModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inspark/courseregistrationmodule/CourseRegistrationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspark.courseregistrationmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRegistrationModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
